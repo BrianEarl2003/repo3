e41f463615728c4a84f1e9e52d2a038ccd366ccd
Brian Earl <ear18009@byui.edu>
2020-04-28 11:01:22 -0600

    in .js changed changecolor()

--- web/02teach.js
+++ web/02teach.js
@@ -3,6 +3,6 @@ function click() {
 }
 
 function changeColor() { 
-    var color = document.getElementById("textColor").innerHTML;
-    document.getElementById("div1").style.background = color;
+    var color = document.getElementById("textColor").value;
+    document.getElementById("div1").style.backgroundColor = color;
 }
