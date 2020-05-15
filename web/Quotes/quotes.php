cb51554bdb2b2ca6454ef165b2e96cf202fec3c9
Brian Earl <ear18009@byui.edu>
2020-05-14 21:28:43 -0600

    "changed course to category"

---
+++ web/Quotes/quotes.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Quotable Quotes</title>
+    <!--for google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Comfortaa|Creepster|Sat\
+isfy&display=swap" rel="stylesheet">
+    <!--CSS modifications-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="quotes.css">
+  </head>
+
+  <body>
+    <!--webpage header-->
+    <header>
+       
+    </header>
+    <hr><!--start-->
+    <form>Random Quote or List from Category<hr>
+      <div class="form-group">
+        <label for="exampleFormControlSelect1">Categories</label>
+        <select class="form-control" id="exampleFormControlSelect1">
+          <option>Humor</option>
+          <option>Inspirational</option>
+          <option>Religious</option>
+          <option>Historical</option>
+          <option>Educational</option>
+        </select>
+      </div>
+    </form>
+
+    <form>
+      <div class="form-group">
+        <label for="exampleFormControlTextarea1">Enter Quote for Submission</label>
+        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+      </div>
+    </form>
+
+    <hr><!--end-->
+    <!--footer-->
+    <footer>
+     
+    </footer>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="quotes.js"></script>
+  </body>
+</html>
