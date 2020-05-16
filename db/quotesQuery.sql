88c0997513dbdb805cf0451365a837b07af96a86
Brian Earl <ear18009@byui.edu>
2020-05-16 14:48:36 -0600

    working on php database requests

--- db/quotesQuery.sql
+++ db/quotesQuery.sql
@@ -9,3 +9,7 @@ SELECT category_name, content FROM category c JOIN quote q ON c.id=q.category_id
 SELECT content FROM category c JOIN quote q ON c.id=q.category_id WHERE c.category_name='Humor';
 
 SELECT content FROM category c, quote q WHERE c.id=q.category_id AND c.category_name='Humor';
+--List of all quotes
+SELECT id, quotee, content FROM quote;
+--Random Quote
+SELECT id, quotee, content FROM quote q WHERE q.id=floor(random()*(7-1+1)+1)::int;
