0928faf28d4e4f204407dde1a5596e09f1370d32
Brian Earl <ear18009@byui.edu>
2020-05-22 13:02:48 -0600

    moved connection to another php using require

---
+++ web/Quotes/DBconnect.php
@@ -0,0 +1,38 @@
+<?php
+    /*try
+    {
+      $dbHost = "ec2-52-20-248-222.compute-1.amazonaws.com";
+      $dbPort = "5432";
+      $dbUser = "erdpcvpqfbmrqq";
+      $dbPassword = "12c451b47dd20c063077806d576d9b73b319d400ced6235190899dd4c9097b7f";
+      $dbName = "d2hm98idujfmai";
+      $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch (PDOException $ex)
+    {
+      echo 'Error!: ' . $ex->getMessage();
+      die();
+    }*/
+    try
+    {
+      $dbUrl = getenv('DATABASE_URL');
+
+      $dbOpts = parse_url($dbUrl);
+
+      $dbHost = $dbOpts["host"];
+      $dbPort = $dbOpts["port"];
+      $dbUser = $dbOpts["user"];
+      $dbPassword = $dbOpts["pass"];
+      $dbName = ltrim($dbOpts["path"],'/');
+
+      $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch (PDOException $ex)
+    {
+      echo 'Error!: ' . $ex->getMessage();
+      die();
+    }
+?>
